57a5a8dd73f0d44b317eaa86713c7174d93d0834
oemer.aran95@gmail.com <oemer.aran95@gmail.com>
2024-10-26 23:07:49 +0200

    feat: ignore eslint on build

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
   experimental: {
     webpackMemoryOptimizations: true
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
